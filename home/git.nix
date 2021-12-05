88a156e236437de16d7263fd4992e796a50f7cff
Nadeem Bitar <nadeem@gmail.com>
2021-12-05 09:28:26 -0800

    📜 Add basic git config

--- home/git.nix
+++ home/git.nix
@@ -6,4 +6,29 @@
   # Aliases config imported in flake.
   programs.git.enable = true;
 
+  programs.git = {
+    userEmail = "nadeem@gmail.com";
+    userName = "Nadeem Bitar";
+
+    extraConfig = {
+      core = {
+        editor = "nvim";
+        ignorecase = false;
+      };
+      rerere.enabled = 1;
+      push = {
+        default = "tracking";
+        followTags = true;
+      };
+      pull = {
+        rebase = true;
+      };
+      apply.whitespace = "nowarn";
+      url = {
+        "https://github.com/" = { insteadOf = "gh:"; };
+        "ssh://git@github.com/" = { insteadOf = "sgh:"; };
+      };
+      init.defaultBranch = "master";
+    };
+  };
 }
