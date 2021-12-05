12b8a78f2e21e5892af23787ad4f2cf3a2ff1a67
Nadeem Bitar <nadeem@gmail.com>
2021-12-05 09:10:53 -0800

    📜 Add basic zsh aliases

--- home/zsh.nix
+++ home/zsh.nix
@@ -5,9 +5,28 @@
   programs.zsh.enable = true;
 
   programs.zsh.shellAliases = with pkgs; {
+    #general 
+    ".." = "cd ..";
+
+    #common cli aliases
+    v = "${neovim}/bin/nvim";
+    view = "v -R";
+    vim = "v";
+
+
+
+    #nix
+    nb = "nix build";
+    nd = "nix develop";
+    nf = "nix flake";
+    nr = "nix run";
+    ns = "nix search";
+
+    #modern cli tools 
     cat = "${bat}/bin/bat";
     du = "${du-dust}/bin/dust";
     ls = "${exa}/bin/exa";
+    ll = "ls -l --time-style long-iso --icons";
     tree = "ls -T";
     ps = "${procs}/bin/procs";
   };
