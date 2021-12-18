d36f8ee2e268cbbc0fe86ccbe48bea293562d8c6
Nadeem Bitar <nadeem@gmail.com>
2021-12-18 12:38:09 -0800

    🚧 Start configuring neovim

---
+++ config/nvim/lua/init.lua
@@ -0,0 +1,8 @@
+-- Aliases
+
+local g = vim.g
+local env = vim.env
+
+
+
+g.mapleader = '<Space>'
--- home/neovim.nix
+++ home/neovim.nix
@@ -1,5 +1,31 @@
 { config, pkgs, lib, ... }:
 
+let
+  inherit (lib) getName mkIf optional;
+  inherit (config.lib.file) mkOutOfStoreSymlink;
+  nixConfigDir = "${config.home.homeDirectory}/.config/dotfiles.nix";
+
+in
 {
   programs.neovim.enable = true;
+
+
+  # Config and plugins {{{
+
+  # minimal init.vim config to load lua config. nix and home manager don't currently support
+  # `init.lua`.
+  xdg.configFile."nvim/lua".source = mkOutOfStoreSymlink "${nixConfigDir}/config/nvim/lua";
+  programs.neovim.extraConfig = "lua require('init')";
+
+  # }}}
+
+  # Required packages {{{
+
+  programs.neovim.extraPackages = with pkgs; [
+    gcc # needed for nvim-treesitter
+    tree-sitter # needed for nvim-treesitter
+  ];
+
+  # }}}
 }
+# vim: foldmethod=marker
