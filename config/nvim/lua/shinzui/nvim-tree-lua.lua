6e29eef796787d3ecbfb38969e15a7ee486c400d
Nadeem Bitar <nadeem@gmail.com>
2021-12-19 11:01:27 -0800

    📜 Add basic nvim-tree config

--- config/nvim/lua/shinzui/nvim-tree-lua.lua
+++ config/nvim/lua/shinzui/nvim-tree-lua.lua
@@ -2,7 +2,10 @@
 --A file explorer tree for neovim written in lua
 --https://github.com/kyazdani42/nvim-tree.lua
 
+local g = vim.g
+
 vim.cmd "packadd nvim-tree.lua"
+g["nvim_tree_quit_on_open"] = 1
 
 require("nvim-tree").setup {
   auto_close = true,
