37328cf24a60af5aab6a449367b2f1f56c6b3261
Nadeem Bitar <nadeem@gmail.com>
2021-12-20 11:24:47 -0800

    📜 Add iohk binary caches

--- darwin/bootstrap.nix
+++ darwin/bootstrap.nix
@@ -8,11 +8,13 @@
   nix.binaryCaches = [
     "https://cache.nixos.org/"
     "https://shinzui.cachix.org"
+    "https://hydra.iohk.io"
   ];
 
   nix.binaryCachePublicKeys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "shinzui.cachix.org-1:QEmAoJrA9WwLP0uxfDgktLi2BRrcvQQWdz8NzcMg4/E="
+    "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
   ];
   nix.trustedUsers = [
     "shinzui"
