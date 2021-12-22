398ae6adb49146e08092f097f7a3b7372d09837d
Nadeem Bitar <nadeem@gmail.com>
2021-12-22 08:16:45 -0800

    📜 Configure lua lsp

--- config/nvim/lua/shinzui/nvim-lspconfig.lua
+++ config/nvim/lua/shinzui/nvim-lspconfig.lua
@@ -21,4 +21,30 @@ lspconf.yamlls.setup {
     },
   },
 }
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+lspconf.sumneko_lua.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 
