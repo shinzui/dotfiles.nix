5c0ca1416c510b6b8975a52d18ec9e476ccba6c8
Nadeem Bitar <nadeem@gmail.com>
2022-01-26 19:07:25 -0800

    📜 Configure eslint diagnostics

--- config/nvim/lua/shinzui/null-ls-nvim.lua
+++ config/nvim/lua/shinzui/null-ls-nvim.lua
@@ -8,7 +8,7 @@ local null_ls = require "null-ls"
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
--- local diagnostics = null_ls.builtins.diagnostics
+local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup {
   debug = false,
@@ -29,5 +29,6 @@ null_ls.setup {
       },
     },
     formatting.stylua,
+    diagnostics.eslint
   },
 }
