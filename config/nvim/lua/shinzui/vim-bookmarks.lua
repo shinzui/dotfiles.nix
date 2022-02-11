df2a907756f4d6033cf2a1b69b54a3c09307b239
Nadeem Bitar <nadeem@gmail.com>
2022-02-11 11:48:58 -0800

    Revert "📦 Add and configurevim-bookmarks"
    This reverts commit 06e68e7ceb9c2be614ef9991c251f0b9102efff2.

--- config/nvim/lua/shinzui/vim-bookmarks.lua
+++
@@ -1,7 +0,0 @@
--- vim-bookmarks
--- https://github.com/MattesGroeger/vim-bookmarks
-vim.cmd "packadd vim-bookmarks"
-
-local g = vim.g
-g.bookmark_save_per_working_dir= 1
-g.bookmarks_auto_save = true
