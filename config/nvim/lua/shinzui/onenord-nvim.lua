46b4776162da4a6aef0dd20fbc1cbf91a86815d6
Nadeem Bitar <nadeem@gmail.com>
2022-06-15 07:10:19 -0700

    Fix cmp highlight color

--- config/nvim/lua/shinzui/onenord-nvim.lua
+++ config/nvim/lua/shinzui/onenord-nvim.lua
@@ -2,7 +2,11 @@
 -- A Neovim theme that combines the Nord and Atom One
 --https://github.com/rmehri01/onenord.nvim
 vim.cmd "packadd onenord-nvim"
+local colors = require("onenord.colors").load()
 
 require("onenord").setup({
-  theme = "dark"
+  theme = "dark",
+  custom_highlights = {
+    CmpItemKindVariable = { fg = colors.dark_blue, style = "bold" }
+  }
 })
