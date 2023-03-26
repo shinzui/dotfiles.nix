08b58587781d8670db1191b255c88f7015eba000
Nadeem Bitar <nadeem@gmail.com>
2023-03-26 08:40:47 -0700

    🔒 📜 Add npmrc

--- darwin/secrets.nix
+++ darwin/secrets.nix
@@ -2,10 +2,18 @@
 
 {
   age.identityPaths = [ "/Users/shinzui/.ssh/id_ed25519" ];
-  age.secrets.netrc = {
-    file = ../secrets/netrc.age;
-    path = "/Users/shinzui/.netrc";
-    mode = "700";
-    owner = "shinzui";
+  age.secrets = {
+    netrc = {
+      file = ../secrets/netrc.age;
+      path = "/Users/shinzui/.netrc";
+      mode = "700";
+      owner = "shinzui";
+    };
+    npmrc = {
+      file = ../secrets/npmrc.age;
+      path = "/Users/shinzui/.npmrc";
+      mode = "700";
+      owner = "shinzui";
+    };
   };
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,9 @@
 let
   shinzui = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFr90yzWnHzUraT2owYt2MR9snqFNhVcP33l4agGJZ7R";
   sungkyung = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIItAXkqDeYi7Ipxhfu0Q2gIjS4EI6FOupN7JSJ4DJjma";
+  shinzuiAtSungkyung = [ shinzui sungkyung ];
 in
 {
-  "netrc.age".publicKeys = [ shinzui sungkyung];
+  "netrc.age".publicKeys = shinzuiAtSungkyung;
+  "npmrc.age".publicKeys = shinzuiAtSungkyung;
 }
