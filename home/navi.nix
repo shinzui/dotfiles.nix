2bf410f31da374a1c9ee4281352d76a220ad977d
Nadeem Bitar <nadeem@gmail.com>
2024-05-08 05:08:47 -0700

    🍬 Update navi link

--- home/navi.nix
+++ home/navi.nix
@@ -7,7 +7,7 @@
     enableZshIntegration = true;
   };
 
-  #https://rycee.gitlab.io/home-manager/options.html#opt-programs.navi.settings
+  #https://nix-community.github.io/home-manager/options.xhtml#opt-programs.navi.enable
   # https://github.com/denisidoro/navi/blob/master/docs/config_file.md#example
   programs.navi.settings = {
     cheats.paths = [
