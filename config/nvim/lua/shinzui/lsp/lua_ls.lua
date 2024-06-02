dc487ee2badd119a9c0fa1a29cc08b2fef26f8e4
Nadeem Bitar <nadeem@gmail.com>
2024-06-02 12:13:18 -0700

    📜 Update lua_ls config

--- config/nvim/lua/shinzui/lsp/lua_ls.lua
+++ config/nvim/lua/shinzui/lsp/lua_ls.lua
@@ -5,6 +5,17 @@ table.insert(runtime_path, "lua/?/init.lua")
 local opts = {
   settings = {
     Lua = {
+      workspace = {
+        checkThirdParty = false,
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      codeLens = {
+        enable = true,
+      },
+      completion = {
+        callSnippet = "Replace",
+      },
       runtime = {
         -- Tell the language server which version of Lua you're using (LuaJIT in the case of Neovim)
         version = "LuaJIT",
@@ -14,10 +25,6 @@ local opts = {
       diagnostics = {
         -- Get the language server to recognize the `vim` global
         globals = { "vim" },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
       },
       telemetry = {
         enable = false,
