802151e3a533ea362dd0e5c4b5d7ea7245331d84
Nadeem Bitar <nadeem@gmail.com>
2024-08-29 12:17:46 -0700

    📜 Update pgcli theme

--- home/external-configs.nix
+++ home/external-configs.nix
@@ -2,4 +2,5 @@
   home.file.".psqlrc".source = ../config/psqlrc;
   home.file."${config.xdg.configHome}/pspg/pspgconf".source=../config/pspg/pspgconf;
   home.file."${config.xdg.configHome}/pspg/.pspg_theme_onenord".source=../config/pspg/pspg_theme_onenord;
+  home.file."${config.xdg.configHome}/pgcli/config".source=../config/pgcli;
 }
