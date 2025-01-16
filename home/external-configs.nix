fa82e838170f00b4a093683faa93ae7cf54a5e09
Nadeem Bitar <nadeem@gmail.com>
2025-01-15 20:27:11 -0800

    📜 Add config for yamllint

--- home/external-configs.nix
+++ home/external-configs.nix
@@ -1,5 +1,6 @@
 { config, ... }: {
   home.file.".psqlrc".source = ../config/psqlrc;
+  home.file."${config.xdg.configHome}/yamllint/config".source=../config/yamllint;
   home.file."${config.xdg.configHome}/pspg/pspgconf".source=../config/pspg/pspgconf;
   home.file."${config.xdg.configHome}/pspg/.pspg_theme_onenord".source=../config/pspg/pspg_theme_onenord;
   home.file."${config.xdg.configHome}/pgcli/config".source=../config/pgcli;
