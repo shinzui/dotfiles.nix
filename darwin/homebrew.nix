b69d7244d4c2b3aa0ab02acc98079c89e5b93cac
Nadeem Bitar <nadeem@gmail.com>
2025-01-16 16:57:06 -0800

    📦 Add ghostty from homebrew

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -54,6 +54,7 @@ in
   # If an app isn't available in the Mac App Store install the Homebrew Cask.
   homebrew.casks = [
     "tuple"
+    "ghostty"
     "zoom"
     "visual-studio-code"
     "discord"
