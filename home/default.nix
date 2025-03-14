51b6f4f701ed7a6ddeb64a4ea1e2751359c2b015
Nadeem Bitar <nadeem@gmail.com>
2025-03-14 16:20:13 -0700

    🔥 Remove sapling

--- home/default.nix
+++ home/default.nix
@@ -130,7 +130,6 @@ in
     pigz # modern gzip
     kondo # clean software project files
     yt-dlp
-    sapling # facebook SCM that's also a git client
     jujutsu # git-compatible VCS
     _1password-cli #1password cli
     duti # select default apps for file types
