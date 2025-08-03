a29776561763a835e41ef668d6dbd6e7353cdf94
Nadeem Bitar <nadeem@gmail.com>
2025-08-03 14:43:17 -0700

    📜 Disable carapace

--- home/carapace.nix
+++ home/carapace.nix
@@ -1,9 +1,9 @@
-{ ... }:
+{ config, ... }:
 
 {
  #https://rycee.gitlab.io/home-manager/options.xhtml#opt-programs.carapace.enable
  programs.carapace = {
-  enable = true;
-  enableZshIntegration = true;
+  enable = false;
+  enableZshIntegration = false;
  };
 }
