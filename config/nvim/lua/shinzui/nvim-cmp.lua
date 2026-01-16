72a84481be4a8bee1b9b091a2e151b6ffdc6fc01
Nadeem Bitar <nadeem@gmail.com>
2026-01-15 16:00:19 -0800

    Change nvim-cmp prev item keybind from C-k to C-p to avoid digraph conflict

--- config/nvim/lua/shinzui/nvim-cmp.lua
+++ config/nvim/lua/shinzui/nvim-cmp.lua
@@ -60,7 +60,7 @@ cmp.setup {
     documentation = cmp.config.window.bordered()
   },
   mapping = cmp.mapping.preset.insert({
-    ["<C-k>"] = cmp.mapping.select_prev_item(),
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-j>"] = cmp.mapping.select_next_item(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
