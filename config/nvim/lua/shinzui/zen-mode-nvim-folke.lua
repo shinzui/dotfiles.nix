086d8d66db42ab56f58fc9b8340edb50b2d34a5e
Nadeem Bitar <nadeem@gmail.com>
2026-02-19 12:21:52 -0800

    Add catppuccin-latte colorscheme and word wrap for zen mode

--- config/nvim/lua/shinzui/zen-mode-nvim-folke.lua
+++ config/nvim/lua/shinzui/zen-mode-nvim-folke.lua
@@ -3,6 +3,9 @@
 -- https://github.com/folke/zen-mode.nvim
 vim.cmd "packadd zen-mode-nvim-folke"
 
+local previous_colorscheme = nil
+local previous_background = nil
+
 require("zen-mode").setup({
   window = {
     backdrop = 0.95,
@@ -18,6 +21,25 @@ require("zen-mode").setup({
       list = false,
     },
   },
+  on_open = function(win)
+    previous_colorscheme = vim.g.colors_name
+    previous_background = vim.o.background
+    vim.o.background = "light"
+    vim.cmd("colorscheme catppuccin-latte")
+    vim.wo[win].wrap = true
+    vim.wo[win].linebreak = true
+  end,
+  on_close = function()
+    vim.defer_fn(function()
+      if previous_colorscheme then
+        vim.cmd("highlight clear")
+        vim.o.background = previous_background
+        vim.cmd("colorscheme " .. previous_colorscheme)
+      end
+      vim.opt.wrap = false
+      vim.opt.linebreak = false
+    end, 50)
+  end,
   plugins = {
     options = {
       enabled = true,
