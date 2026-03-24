6319feac5b142bd64056ceab89c3ddfdef87c6cc
Nadeem Bitar <nadeem@gmail.com>
2026-03-23 20:13:30 -0700

    Add kizamu zsh shell completions

--- home/kizamu.nix
+++ home/kizamu.nix
@@ -1,7 +1,16 @@
 { pkgs, ... }:
 
+let
+  kizamuBin = "${pkgs.kizamu}/bin/kizamu";
+
+  kizamu-zsh-completions = pkgs.runCommand "kizamu-zsh-completions" { } ''
+    ${kizamuBin} completions zsh > $out
+  '';
+in
 {
   home.packages = [
     pkgs.kizamu
   ];
+
+  home.file.".zfunc/_kizamu".source = kizamu-zsh-completions;
 }
