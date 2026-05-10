799654661c5c0e65471a5a2b92f88272c37516ef
Nadeem Bitar <nadeem@gmail.com>
2026-05-10 10:25:20 -0700

    chore(hunk): update to 0.11.1

--- derivations/hunk/default.nix
+++ derivations/hunk/default.nix
@@ -6,28 +6,28 @@
 }:
 
 let
-  version = "0.10.0";
+  version = "0.11.1";
   src = fetchurl {
     url = "https://registry.npmjs.org/hunkdiff/-/hunkdiff-${version}.tgz";
-    hash = "sha512-GfUYNCzEnZ0OTdg340YRFbW1SvvwgRMyQmn44t2GKoSjYqiXGaDCeOG66fpIzU8WRdbUi2uzdGIVkEsCps8TeA==";
+    hash = "sha512-3yI7YLMGSC2k+gj9mOnAhYPKGZCVyQOJ/nGRwKs23VP1ahs2ov1GIX3NwhxxzlT+uftRB1kZeW3CtRAeshKQhw==";
   };
 
   platformPackages = {
     aarch64-darwin = {
       name = "hunkdiff-darwin-arm64";
-      hash = "sha512-oJALanUcIFp19LQbTTNKEk/RA0QIeeqwXzUciTzBlze1IA5GPe+rq+OLy66fFUA5tiO6qj6sXf1UqK9cL8o0Mw==";
+      hash = "sha512-0uTuJeB7ZrT999WMcvUU1YvvIgoHIrU5orObKvXc5/Ach+tZWwt85cKD3Sy8sk0QLd/KoGEgRODSKFl5Tjx4+A==";
     };
     x86_64-darwin = {
       name = "hunkdiff-darwin-x64";
-      hash = "sha512-5sVwIN7OQ4x6/K1TfP4n0wUZinL9nPKmbZ/oHJWhMD6FScGuOOYYZQtN+q2j3ahzlu36Iio7OXajuyQZulwU4A==";
+      hash = "sha512-vmk97ifp08kVUvuAXluMFT+o3bKDI/H4HVPnEvRe+XINEN2QKJrw8GWQXL2axjy3hPpNKZsstuuAJM+UEfIiIg==";
     };
     aarch64-linux = {
       name = "hunkdiff-linux-arm64";
-      hash = "sha512-h3yY1cxEmer3StCppvQ4kZyK10971t6dMO76jMnWNhREWML2H2hCiPrNw5Yjx0tI0AyI1P4D3guNCcvylLmO4A==";
+      hash = "sha512-g+3hs/ffKRL+TVTck1AVKhC3ym0UxMh6yVEXyg/FPnwDOOKgaPMMRpN7HLI6qzgdL7lZBcmXFw/bI6XrP2a93A==";
     };
     x86_64-linux = {
       name = "hunkdiff-linux-x64";
-      hash = "sha512-me3Pl6Tqb46yoZP930iCUdE3pE5lDOtfsWUcCZXqEpsg0WPbW6PjO6tjX7MRnkLFPacPDrqfPZpEHr2bxK0X9A==";
+      hash = "sha512-2a0bDS0IbjoLc6zzdK2A/0O73Uhze8/kzXcJyu59meBXnk3hJGZxKuwoFW4v/g0hOnnmvR6OPhmiLEMGV/Jy+Q==";
     };
   };
 
